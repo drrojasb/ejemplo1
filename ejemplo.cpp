1efb65aa1c66ff476882231b2502978948a86e12
David Ricardo Rojas <drrojasb@unal.edu.co>
2021-10-14 13:57:40 -0500

    Ejemplo de c++

---
+++ ejemplo.cpp
@@ -0,0 +1,11 @@
+// Calcular los numeros primos entre 1 y 100
+
+// Incluir las librerías
+#include <iostream>
+
+//main function
+int main (void)
+{
+  std::cout << "Hello world!"<< "";
+  return 0;
+}
