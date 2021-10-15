cd5802a37609f3e016b43ce18e2fe943981ac110
David Ricardo Rojas <drrojasb@unal.edu.co>
2021-10-15 08:59:32 -0500

    hola mundo

---
+++ holamundo.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //el caracter '#' indica una llamada al preprocesador 
+
+int main ()
+{
+  int m = 2;
+  int n = 100;
+  for (int ii = m; ii)
+  std::cout << "Hola mundo";
+  return 0;
+}
+//Compilación: 'g++ holamundo.cpp' compila el código y crea un ejecutable llamado 'a.out' por defecto
+//Ejecución: './a.out'
+
+//programación c++//
+
+//variables: int, string, float, double, bool, char
+//operadores: +, -, *, /, %
+//funciones:
+//estructuras de datos:
